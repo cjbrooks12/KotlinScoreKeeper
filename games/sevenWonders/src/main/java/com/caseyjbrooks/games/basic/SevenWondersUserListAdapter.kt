7bf9f7cccf67fa3d1959014be4bd0d7eb2c5b931
Casey Brooks <cjbrooks12@gmail.com>
2017-12-24 15:20:11 -0600

    finishes seven wonders game functionality

--- games/sevenWonders/src/main/java/com/caseyjbrooks/games/basic/SevenWondersUserListAdapter.kt
+++ games/sevenWonders/src/main/java/com/caseyjbrooks/games/basic/SevenWondersUserListAdapter.kt
@@ -1,6 +1,7 @@
 package com.caseyjbrooks.games.basic
 
 import android.graphics.Color
+import android.text.InputType
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +11,9 @@ import com.caseyjbrooks.scorekeeper.core.api.BaseActivity
 import com.caseyjbrooks.scorekeeper.core.api.BaseComponent
 import com.caseyjbrooks.scorekeeper.core.api.BaseGameAdapter
 import org.jetbrains.anko.*
+import org.jetbrains.anko.appcompat.v7.Appcompat
 import org.jetbrains.anko.cardview.v7.cardView
+import org.jetbrains.anko.sdk25.coroutines.onClick
 import org.jetbrains.anko.sdk25.coroutines.onLongClick
 
 class SevenWondersUserListAdapter(
@@ -21,8 +24,9 @@ class SevenWondersUserListAdapter(
 
     override fun getView(i : Int, v : View?, parent : ViewGroup?) : View {
         val vm = getItem(i)
+        val ankoContext = AnkoContext.create(parent!!.context, this)
 
-        return with(parent!!.context) {
+        return with(ankoContext) {
             frameLayout {
                 cardView {
                     cardElevation = dip(2).toFloat()
@@ -31,7 +35,7 @@ class SevenWondersUserListAdapter(
                     verticalLayout {
                         textView(vm.user.name) {
                             onLongClick {
-                                gameViewModel.renameUser(AnkoContext.create(parent.context, this), vm.user)
+                                gameViewModel.renameUser(ankoContext, vm.user)
                             }
 
                             gravity = Gravity.CENTER
@@ -52,7 +56,7 @@ class SevenWondersUserListAdapter(
                                 gravity = Gravity.CENTER
                                 textSize = dip(18).toFloat()
 
-                                text = "0"
+                                text = "${vm.getScore()}"
                             }
                         }.lparams {
                             width = matchParent
@@ -67,6 +71,66 @@ class SevenWondersUserListAdapter(
                             margin = dip(8)
                         }
 
+                        linearLayout {
+                            weightSum = 7.0f
+
+                            button("${vm.coin}") {
+                                backgroundColor = Color.parseColor("#FFCF3A")
+                                onClick { updateScore(ankoContext, "coin", vm) }
+                            }.lparams {
+                                height = dip(48)
+                                weight = 1.0f
+                            }
+
+                            button("${vm.wonder}") {
+                                backgroundColor = Color.parseColor("#FFF481")
+                                onClick { updateScore(ankoContext, "wonder", vm) }
+                            }.lparams {
+                                height = dip(48)
+                                weight = 1.0f
+                            }
+
+                            button("${vm.military}") {
+                                backgroundColor = Color.parseColor("#B56965")
+                                onClick { updateScore(ankoContext, "military", vm) }
+                            }.lparams {
+                                height = dip(48)
+                                weight = 1.0f
+                            }
+
+                            button("${vm.civilian}") {
+                                backgroundColor = Color.parseColor("#6275C9")
+                                onClick { updateScore(ankoContext, "civilian", vm) }
+                            }.lparams {
+                                height = dip(48)
+                                weight = 1.0f
+                            }
+
+                            button("${vm.commercial}") {
+                                backgroundColor = Color.parseColor("#FFB700")
+                                onClick { updateScore(ankoContext, "commercial", vm) }
+                            }.lparams {
+                                height = dip(48)
+                                weight = 1.0f
+                            }
+
+                            button("${vm.guild}") {
+                                backgroundColor = Color.parseColor("#619968")
+                                onClick { updateScore(ankoContext, "guild", vm) }
+                            }.lparams {
+                                height = dip(48)
+                                weight = 1.0f
+                            }
+
+                            button("${vm.science}") {
+                                backgroundColor = Color.parseColor("#614B75")
+                                onClick { updateScore(ankoContext, "science", vm) }
+                            }.lparams {
+                                height = dip(48)
+                                weight = 1.0f
+                            }
+                        }
+
                     }.layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT)
                 }.lparams {
                     width = matchParent
@@ -79,6 +143,30 @@ class SevenWondersUserListAdapter(
 
     override fun getItemId(position : Int) : Long {
         return getItem(position).id
+    }
+
+    fun <UI> updateScore(ui: AnkoContext<UI>, scoreType: String, user: SevenWondersUserViewModel) = with(ui) {
+        alert(Appcompat, title = "${scoreType.capitalize()} - ${user.user.name}", message = "") {
+            customView {
+                verticalLayout {
+                    val name = editText {
+                        hint = "${scoreType.capitalize()} score"
+                        inputType = InputType.TYPE_CLASS_NUMBER
+                    }
+                    positiveButton("Save") {
+                        if(name.text.isNotEmpty()) {
+                            val score = Integer.parseInt(name.text.toString())
+                            user.setScoreItem(scoreType, score)
+                            user.saveUser()
+                        }
+                        else {
+                            toast("Score cannot be empty")
+                        }
+                    }
+                    negativeButton("Cancel") { }
+                }
+            }
+        }.show()
     }
 
 }
--- games/sevenWonders/src/main/java/com/caseyjbrooks/games/basic/SevenWondersUserViewModel.kt
+++ games/sevenWonders/src/main/java/com/caseyjbrooks/games/basic/SevenWondersUserViewModel.kt
@@ -15,14 +15,66 @@ class SevenWondersUserViewModel(
 
     val id: Long = user.id
 
-    override fun initFromData(savedData: JSONObject) {
+    var coin: Int = 0
+    var wonder: Int = 0
+    var military: Int = 0
+    var civilian: Int = 0
+    var commercial: Int = 0
+    var guild: Int = 0
+    var science: Int = 0
 
+    override fun initFromData(savedData: JSONObject) {
+        coin       = if(savedData.has("coin"))       { savedData.getInt("coin")       } else { 0 }
+        wonder     = if(savedData.has("wonder"))     { savedData.getInt("wonder")     } else { 0 }
+        military   = if(savedData.has("military"))   { savedData.getInt("military")   } else { 0 }
+        civilian   = if(savedData.has("civilian"))   { savedData.getInt("civilian")   } else { 0 }
+        commercial = if(savedData.has("commercial")) { savedData.getInt("commercial") } else { 0 }
+        guild      = if(savedData.has("guild"))      { savedData.getInt("guild")      } else { 0 }
+        science    = if(savedData.has("science"))    { savedData.getInt("science")    } else { 0 }
     }
 
     override fun getSaveData() : JSONObject {
         val data = JSONObject()
 
+        data.put("coin", coin)
+        data.put("wonder", wonder)
+        data.put("military", military)
+        data.put("civilian", civilian)
+        data.put("commercial", commercial)
+        data.put("guild", guild)
+        data.put("science", science)
+
         return data
+    }
+
+    fun setScoreItem(scoreType: String, score: Int) {
+        when(scoreType) {
+            "coin" -> coin = score
+            "wonder" -> wonder = score
+            "military" -> military = score
+            "civilian" -> civilian = score
+            "commercial" -> commercial = score
+            "guild" -> guild = score
+            "science" -> science = score
+        }
+
+        saveUser()
+    }
+
+    override fun reset() {
+        coin       = 0
+        wonder     = 0
+        military   = 0
+        civilian   = 0
+        commercial = 0
+        guild      = 0
+        science    = 0
+
+        saveUser()
+    }
+
+    fun getScore(): Int {
+        return coin + wonder + military + civilian + commercial + guild + science
     }
 
 }
